1a7137e4a29efdf682ec197a872340015f28b411
mashalit <mashal_it@yahoo.com>
2020-03-31 18:47:55 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mashal");
+            Console.ReadKey();
+
+            int _verabl001 = 7;
+            Console.Write( _verabl001);
+
+            Console.WriteLine("");
+            Console.ReadKey();
+
+            Console.WriteLine("the end");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
